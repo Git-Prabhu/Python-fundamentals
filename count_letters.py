ddf93159b0b24184ba23ae8e85887147af414bbe
Git-Prabhu <pcube@hotmail.com>
2018-06-27 18:33:08 -0400

    Add files via upload

---
+++ count_letters.py
@@ -0,0 +1,11 @@
+def count_vowels(word):
+    '''(str) ->int
+
+    Return the number of vowels in word.
+    >>>count_vowels('hello')
+    2
+    >>>count_vowels('bonjour')
+    3
+    '''
+
+    return count(word)
